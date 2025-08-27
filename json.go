e9f833e8fef3cd63744fd9ead357fb24ca3d4f36
Arsalan Yahyazadeh <arsalan.jhnson@gmail.com>
2025-08-27 21:00:15 +0300

    fix security bugs

--- json.go
+++ json.go
@@ -28,7 +28,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
 		return
-	}
-	w.WriteHeader(code)
-	w.Write(dat)
+	}        w.WriteHeader(code)
+        if _, err := w.Write(dat); err != nil {
+                log.Printf("Error writing response: %v", err)
+        }
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -85,13 +86,15 @@ func main() {
 		v1Router.Post("/notes", apiCfg.middlewareAuth(apiCfg.handlerNotesCreate))
 	}
 
-	v1Router.Get("/healthz", handlerReadiness)
-
-	router.Mount("/v1", v1Router)
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-	}
+	v1Router.Get("/healthz", handlerReadiness)        router.Mount("/v1", v1Router)
+        srv := &http.Server{
+                Addr:              ":" + port,
+                Handler:           router,
+                ReadHeaderTimeout: 10 * time.Second,
+                ReadTimeout:       30 * time.Second,
+                WriteTimeout:      30 * time.Second,
+                IdleTimeout:       120 * time.Second,
+        }
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
